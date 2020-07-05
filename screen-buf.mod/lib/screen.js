91e6b2f6578277361228be6f2d6db66411a7ba36
Igor Khotin <khotin@gmx.com>
2020-07-06 01:25:27 +0300

    [+] face command

--- screen-buf.mod/lib/screen.js
+++ screen-buf.mod/lib/screen.js
@@ -1,21 +1,23 @@
 const palette = [
-    '#000000',
-    '#f4f4f4',
-    '#1a1c2c',
-    '#5d275d',
-    '#b13e53',
-    '#ef7d57',
-    '#ffcd75',
-    '#a7f070',
-    '#38b764',
-    '#257179',
-    '#29366f',
-    '#3b5dc9',
-    '#41a6f6',
-    '#73eff7',
-    '#94b0c2',
-    '#566c86',
-    '#333c57',
+    '#000000', // black
+    '#f4f4f4', // almost white
+    '#1a1c2c', // bluish black
+    '#5d275d', // purple
+    '#b13e53', // red
+    '#ef7d57', // orange
+    '#ffcd75', // yellow
+    '#d1cf94', // pale yellow paper
+    '#a7f070', // salad
+    '#38b764', // green
+    '#257179', // teal
+    '#29366f', // dark blue
+    '#3b5dc9', // blue
+    '#41a6f6', // sky
+    '#73eff7', // cyan
+    '#94b0c2', // metal
+    '#566c86', // dark metal
+    '#333c57', // grayish blue
+    '#252527', // dark face
 ]
 
 function mapColor(ci) {
@@ -37,7 +39,12 @@ const screen = {
     background: function(ci) {
         let c = mapColor(ci)
         ctx.fillStyle = c
-        ctx.fillRect(0, 0, rx(.5), ry(.5))
+        ctx.fillRect(0, 0, rx(1), ry(1))
+    },
+
+    face: function(ci) {
+        let c = mapColor(ci)
+        env.tune.face = c
     },
 
     plot: function(x, y, ci) {
